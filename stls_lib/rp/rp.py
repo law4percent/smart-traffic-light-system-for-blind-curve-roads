68baa6a113b65ae0841a13aa5f45d9b070598c55
Lawrence Roble <lawrence7roble@gmail.com>
2025-01-26 23:14:33 +0800

    Adding mqtt communication method

--- stls_lib/rp/rp.py
+++ stls_lib/rp/rp.py
@@ -2,22 +2,51 @@ import cv2
 from stls_lib import stls
 import time
 from picamera2 import Picamera2
+import paho.mqtt.client as mqtt
 
-def main(weight_file_path: str, class_list_file_path: str, zones_file_path: str, detect_sensivity: float, time_interval: float, frame_name: str, frame_height: int, frame_width: int, wait_key: int, ord_key: str):
+
+def on_publish(client, userdata, mid):
+    print("message published")
+
+
+def main(weight_file_path: str,
+         class_list_file_path: str,
+         zones_file_path: str,
+         detect_sensitivity: float,
+         time_interval: float,
+         frame_name: str,
+         frame_height: int,
+         frame_width: int,
+         wait_key: int,
+         ord_key: str,
+         mqtt_broker : str, # "192.168.1.100" Replace with Raspberry Pi's static IP
+         mqtt_port: int # 1883 default port
+         ):
+    
+    # Initialize camera
     picam2 = Picamera2()
-    picam2.preview_configuration.main.size = (frame_width,frame_height)
+    picam2.preview_configuration.main.size = (frame_width, frame_height)
     picam2.preview_configuration.main.format = "RGB888"
     picam2.preview_configuration.align()
     picam2.configure("preview")
     picam2.start()
     
+    # Load YOLO model and configurations
     yolo_model = stls.load_model(weight_file_path)
     class_list = stls.load_class_names(class_list_file_path)
     zones, number_of_zones = stls.load_zones(zones_file_path)
 
+    topic_esp = ["esp32/one", "esp32/two"]
+    client = mqtt.Client("RaspberryPi")
+    client.connect(mqtt_broker, mqtt_port)
+    client.on_publish = on_publish
+    client.loop_start()  # Start the MQTT loop for handling background tasks
+
+    # Initialize zones and tracking data
     count = 0
     success = True
     zones_data = [{"countdown_start_time": 0.0, "refresh": False, "get_vehicle": None} for _ in range(number_of_zones)]
+    previous_vehicle = [None, None]
 
     while success:
         current_time = time.time()
@@ -29,17 +58,27 @@ def main(weight_file_path: str, class_list_file_path: str, zones_file_path: str,
 
         zones_list = stls.init_zone_list(number_of_zones)
         frame = cv2.resize(frame, (frame_width, frame_height))
-        boxes = stls.get_prediction_boxes(frame, yolo_model, detect_sensivity)
-        stls.draw_polylines_zones(frame, zones, frame_name) # Optional
+        boxes = stls.get_prediction_boxes(frame, yolo_model, detect_sensitivity)
+        stls.draw_polylines_zones(frame, zones, frame_name)  # Optional
         zones_list = stls.track_objects_in_zones(frame, boxes, class_list, zones, zones_list, frame_name)
 
         queuing_data = []
         for indx in range(number_of_zones):
             queuing_data.append(stls.handle_zone_queuing(indx, zones_list, current_time, frame, zones_data, time_interval))
+            
+            # Publish data
+            curr_time = queuing_data[indx]["current_time"]
+            curr_vehic = queuing_data[indx]["vehicle"]
+            if curr_time != 0.0:
+                if previous_vehicle[indx] != curr_vehic:
+                    pubMsg = client.publish(topic_esp[indx], curr_vehic.encode('utf-8'), qos=0)
+                    pubMsg.wait_for_publish()
+                    previous_vehicle[indx] = curr_vehic
 
-        stls.display_zone_info(frame, number_of_zones, zones_list, frame_name, queuing_data) # Optional
-        
-        success = stls.show_frame(frame, frame_name, wait_key, ord_key) # Optional
-        
-    # captured.release()
+        stls.display_zone_info(frame, number_of_zones, zones_list, frame_name, queuing_data)  # Optional
+        success = stls.show_frame(frame, frame_name, wait_key, ord_key)  # Optional
+
+    # Cleanup
+    client.loop_stop()
+    client.disconnect()
     cv2.destroyAllWindows()
